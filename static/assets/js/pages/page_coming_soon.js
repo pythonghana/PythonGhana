b1d529f1f327415c7618f00df9edf1b0cd66449a
mawy7 <mannieyoung7@gmail.com>
2019-02-15 01:47:46 +0000

    New Updates 2019

---
+++ static/assets/js/pages/page_coming_soon.js
@@ -0,0 +1,9 @@
+
+var PageComingSoon = function () {
+    return {
+      //Coming Soon
+      initPageComingSoon: function () {
+            $('#defaultCountdown').countdown({until: new Date(2017, 03-1, 1, 06, 00, 00)})
+        }
+    };
+}();
